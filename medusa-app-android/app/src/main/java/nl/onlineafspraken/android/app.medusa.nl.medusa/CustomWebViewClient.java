05fbcdf3f97ace5988920a73afdfbf842f4bddae
mizarit <ricardo.matters@mizar-it.nl>
2015-04-20 11:23:51 +0200

    Initial import

---
+++ medusa-app-android/app/src/main/java/nl/onlineafspraken/android/app.medusa.nl.medusa/CustomWebViewClient.java
@@ -0,0 +1,21 @@
+package app.medusa.nl.medusa;
+
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class CustomWebViewClient extends WebViewClient {
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url)
+	{
+         view.loadUrl(url);
+         return true;
+    }
+
+    @Override
+    public void onPageFinished(WebView view, String url) {
+
+    }
+}
+
+
+
